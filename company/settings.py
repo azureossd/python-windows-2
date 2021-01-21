e4121f0299ce99760e68c7de1f314ec6fda3a4f8
Edison Garcia <egachi@live.com>
2021-01-21 14:16:16 -0600

    Initial Commit

--- company/settings.py
+++ company/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=kpli-kjn@mk+-^ng+d%-frxc@c-_qr^i_6(+)m+ytm!11ue6@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
