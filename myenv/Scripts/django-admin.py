95bd4e455ab46dc43a99d66d455aa59022535748
Daniel Njoku <njokdan@gmail.com>
2023-02-09 18:46:55 +0000

    just an update

---
+++ myenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\njokdan\Desktop\projects\python-windows-2\myenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
